30d51a1ec1a1b8e5316fc89a77d25e17de3f94c9
Tommy Jeff <threcius@yahoo.com>
2025-12-11 17:16:26 +0800

    Pass allowed extensions as a array to choose file

--- ext/cocoafile/cocoafile.m
+++ ext/cocoafile/cocoafile.m
@@ -15,14 +15,28 @@ Init_cocoafile(void)
 }
 
 VALUE choose_file(VALUE self, VALUE rb_ext) {
-  // Convert Ruby string to NSString
-  Check_Type(rb_ext, T_STRING);
-  const char *ext_cstr = StringValueCStr(rb_ext);
-  NSString *ext = [NSString stringWithUTF8String:ext_cstr];
+  // Ensure the input is an array of strings
+  Check_Type(rb_ext, T_ARRAY);
+  NSMutableArray *extensions = [NSMutableArray array];
+
+  // Convert each Ruby string (extension) to NSString and add to the list
+  long len = RARRAY_LEN(rb_ext);
+  for (long i = 0; i < len; i++) {
+    VALUE ext_val = rb_ary_entry(rb_ext, i);
+    Check_Type(ext_val, T_STRING);
+    const char *ext_cstr = StringValueCStr(ext_val);
+    NSString *ext = [NSString stringWithUTF8String:ext_cstr];
+    
+    UTType *lumioseType = [UTType typeWithFilenameExtension:ext];
+    if (lumioseType) {
+      [extensions addObject:lumioseType];
+    }
+  }
+
+
   NSOpenPanel *panel = [NSOpenPanel openPanel];
 
-  UTType *lumioseType = [UTType typeWithFilenameExtension:ext];
-  panel.allowedContentTypes = @[lumioseType];
+  panel.allowedContentTypes = extensions;
   panel.canChooseFiles = YES;
   panel.canChooseDirectories = NO;
   panel.allowsMultipleSelection = NO;
